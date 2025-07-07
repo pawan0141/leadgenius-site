146c217a65f7c0fb945375ef80059db38bb5527d
pawan0141 <pwnsat0141@gmail.com>
2025-07-07 13:22:27 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button } from "../components/ui/button";
-import { Card, CardContent } from "../components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { ArrowUp } from "lucide-react";
 
 export default function LeadGenAgency() {
